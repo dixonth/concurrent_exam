e4d127fcd13de52d928f8d1e20494d66c33c0464
Thomas <dixonth@tcd.ie>
2021-05-21 15:02:32 +0100

    tidy up and comments

--- csu33014-annual-q1/csu33014-annual-q1-code.c
+++ csu33014-annual-q1/csu33014-annual-q1-code.c
@@ -69,33 +69,32 @@ float routine_1(float * restrict a, float * restrict b,
 // in the following, size can have any positive value
 float vectorized_1(float * restrict a, float * restrict b,
 		   int size) {
-  if(size >= 4) {
-    float * sum_a_output = malloc(sizeof(float)*4);       
+  if(size >= 4) {                                          //check if large enough to vectorize
+    float * sum_a_output = malloc(sizeof(float)*4);        //allocate memory for output vector
     float * sum_b_output = malloc(sizeof(float)*4);
-    __m128 sum_a_vec = _mm_set1_ps(0.0f);
-    __m128 sum_b_vec = _mm_set1_ps(0.0f);
-    __m128 a_vec;
-    __m128 b_vec;
-    int num_even = size - size%4;
+    __m128 sum_a_vector = _mm_set1_ps(0.0f);               //initalise sum vectors with all 0s
+    __m128 sum_b_vector = _mm_set1_ps(0.0f);
+    __m128 a_vector, b_vector;
+    int num_even = size - size%4;                          //calculate number of iterations
     for(int i = 0; i < num_even; i+=4) {
-      a_vec = _mm_load_ps(&a[i]);
-      b_vec = _mm_load_ps(&b[i]);
-      sum_a_vec = _mm_add_ps(sum_a_vec, a_vec);
-      sum_b_vec = _mm_add_ps(sum_b_vec, b_vec);
+      a_vector = _mm_load_ps(&a[i]);                       //load vectors
+      b_vector = _mm_load_ps(&b[i]);
+      sum_a_vector = _mm_add_ps(sum_a_vector, a_vector);   //vector addition
+      sum_b_vector = _mm_add_ps(sum_b_vector, b_vector);         
     }
-    _mm_store_ps(sum_a_output, sum_a_vec);
-    _mm_store_ps(sum_b_output, sum_b_vec);
-    float sum_a = sum_a_output[0]+sum_a_output[1]+sum_a_output[2]+sum_a_output[3];
+    _mm_store_ps(sum_a_output, sum_a_vector);
+    _mm_store_ps(sum_b_output, sum_b_vector);
+    float sum_a = sum_a_output[0]+sum_a_output[1]+sum_a_output[2]+sum_a_output[3];  //combine vectors
     float sum_b = sum_b_output[0]+sum_b_output[1]+sum_b_output[2]+sum_b_output[3];
-    for(int i = num_even; i < size; i++) {
+    for(int i = num_even; i < size; i++) {                 //deal with remainder (%4)
       sum_a += a[i];
       sum_b += b[i];
     }
-    free(sum_a_output);
+    free(sum_a_output);                                    //free memory
     free(sum_b_output);
     return sum_a * sum_b;
   }
-  else {
+  else {                                                   //if not big enough to vectorise, use old function
     float sum_a = 0.0;
     float sum_b = 0.0;
     for ( int i = 0; i < size; i++ ) {
@@ -118,16 +117,16 @@ void routine_2(float * restrict a, float * restrict b, int size) {
 
 void vectorized_2(float * restrict a, float * restrict b, int size) {
   // replace the following code with vectorized code
-  __m128 b_vec;
-  __m128 our_num = _mm_set1_ps(1.5379f);
-  int num_even = size - size%4;
+  __m128 b_vector;
+  __m128 our_num = _mm_set1_ps(1.5379f);              //initalise our_num vector with all 1.5379s
+  int num_even = size - size%4;                       //calculate number of iterations
   for(int i = 0; i < num_even; i+=4) {
-    b_vec = _mm_load_ps(&b[i]);
-    b_vec = _mm_rcp_ps(b_vec);
-    b_vec = _mm_sub_ps(our_num, b_vec);
-    _mm_store_ps(&a[i], b_vec);
+    b_vector = _mm_load_ps(&b[i]);
+    b_vector = _mm_rcp_ps(b_vector);                  //reciprocal b_vector
+    b_vector = _mm_sub_ps(our_num, b_vector);         //our_num - reciprocal
+    _mm_store_ps(&a[i], b_vector);
   }
-  for ( int i = 0; i < size; i++ ) {
+  for ( int i = 0; i < size; i++ ) {                  //deal with remainder (%4)
     a[i] = 1.5379 - (1.0/b[i]);
   }
 }
@@ -146,21 +145,18 @@ void routine_3(float * restrict a, float * restrict b, int size) {
 
 void vectorized_3(float * restrict a, float * restrict b, int size) {
   // replace the following code with vectorized code
-  __m128 a_vec;
-  __m128 b_vec;
-  __m128 a_mask;
-  __m128 b_mask;
+  __m128 a_vector, b_vector, a_mask, b_mask;
 
   int num_even = size - size%4;
   for(int i = 0; i < num_even; i+=4) {
-    a_vec = _mm_load_ps(&a[i]);
-    b_vec = _mm_load_ps(&b[i]);
-    a_mask = _mm_cmpge_ps(a_vec, b_vec);
-    b_mask = _mm_cmplt_ps(a_vec, b_vec);
-    a_vec = _mm_and_ps(a_vec, a_mask);
-    b_vec = _mm_and_ps(b_vec, b_mask);
-    a_vec = _mm_or_ps(a_vec, b_vec);
-    _mm_store_ps(&a[i], a_vec);
+    a_vector = _mm_load_ps(&a[i]);
+    b_vector = _mm_load_ps(&b[i]);
+    a_mask = _mm_cmpge_ps(a_vector, b_vector);
+    b_mask = _mm_cmplt_ps(a_vector, b_vector);
+    a_vector = _mm_and_ps(a_vector, a_mask);
+    b_vector = _mm_and_ps(b_vector, b_mask);
+    a_vector = _mm_or_ps(a_vector, b_vector);
+    _mm_store_ps(&a[i], a_vector);
   }
   for ( int i = 0; i < size; i++ ) {
     if ( a[i] < b[i] ) {
@@ -185,22 +181,22 @@ void routine_4(float * restrict a, float * restrict b,
 void vectorized_4(float * restrict a, float * restrict b,
 		    float * restrict  c) {
   // replace the following code with vectorized code
-  __m128 a_vec;
-  __m128 b_vec;
-  __m128 c_vec;
-  __m128 prod_1;
-  __m128 prod_2;
+  __m128 a_vector;
+  __m128 b_vector;
+  __m128 c_vector;
+  __m128 product_1;
+  __m128 product_2;
   for(int i = 0; i < 2048; i+=4) {
-    b_vec = _mm_load_ps(&b[i]);
-    c_vec = _mm_load_ps(&c[i]);
-    prod_1 = _mm_mul_ps(b_vec, c_vec);
-    prod_1 = _mm_hsub_ps(prod_1, prod_1);
-    c_vec = _mm_shuffle_ps(c_vec, c_vec, _MM_SHUFFLE(2, 3, 0, 1));
-    prod_2 = _mm_mul_ps(b_vec, c_vec);
-    prod_2 = _mm_hadd_ps(prod_2, prod_2);
-    a_vec = _mm_shuffle_ps(prod_2, prod_1, _MM_SHUFFLE(3, 2, 3, 2));
-    a_vec = _mm_shuffle_ps(a_vec, a_vec, _MM_SHUFFLE(3, 1, 2, 0));
-    _mm_store_ps(&a[i], a_vec);
+    b_vector = _mm_load_ps(&b[i]);
+    c_vector = _mm_load_ps(&c[i]);
+    product_1 = _mm_mul_ps(b_vector, c_vector);
+    product_1 = _mm_hsub_ps(product_1, product_1);
+    c_vector = _mm_shuffle_ps(c_vector, c_vector, _MM_SHUFFLE(2, 3, 0, 1));
+    product_2 = _mm_mul_ps(b_vector, c_vector);
+    product_2 = _mm_hadd_ps(product_2, product_2);
+    a_vector = _mm_shuffle_ps(product_2, product_1, _MM_SHUFFLE(3, 2, 3, 2));
+    a_vector = _mm_shuffle_ps(a_vector, a_vector, _MM_SHUFFLE(3, 1, 2, 0));
+    _mm_store_ps(&a[i], a_vector);
   }
 }
 
@@ -223,13 +219,13 @@ int vectorized_5(unsigned char * restrict a,
   int num_even_floats = num_even / 4;
   float * a_as_floats = (float *) b;
   float * b_as_floats = (float *) a;
-  __m128 a_vec;
-  __m128 b_vec;
+  __m128 a_vector;
+  __m128 b_vector;
   int result = 1;
   for(int i = 0; i < num_even_floats; i+=4) {
-    a_vec = _mm_load_ps(&a_as_floats[i]);
-    b_vec = _mm_load_ps(&b_as_floats[i]);
-    __m128 r = _mm_cmpeq_ps(a_vec, b_vec);
+    a_vector = _mm_load_ps(&a_as_floats[i]);
+    b_vector = _mm_load_ps(&b_as_floats[i]);
+    __m128 r = _mm_cmpeq_ps(a_vector, b_vector);
     if( _mm_movemask_ps(r) != 0xF) result = 0;
   }
   a = (unsigned char * restrict) a_as_floats;
@@ -266,26 +262,26 @@ void vectorized_6(float * restrict a, float * restrict b,
     a[i] = sum;
   }
   
-  __m128 b_1;
-  __m128 b_2;
-  __m128 c_1 = _mm_setr_ps(c[0], c[1], c[2], 0.0f);
-  __m128 c_2 = _mm_setr_ps(0.0f, c[0], c[1], c[2]);
-  __m128 prod_1;
-  __m128 prod_2;
+  __m128 b_vector_1;
+  __m128 b_vector_2;
+  __m128 c_vector_1 = _mm_setr_ps(c[0], c[1], c[2], 0.0f);
+  __m128 c_vector_2 = _mm_setr_ps(0.0f, c[0], c[1], c[2]);
+  __m128 product_1;
+  __m128 product_2;
   __m128 sum_1;
   __m128 sum_2;
-  __m128 a_vec;
+  __m128 a_vector;
   for(int i = 4; i < 1020; i+=4) {
-    b_1 = _mm_setr_ps(b[i-1], b[i], b[i+1], b[i+2]);
-    b_2 = _mm_setr_ps(b[i+1], b[i+2], b[i+3], b[i+4]);
-    prod_1 = _mm_mul_ps(b_1, c_1);
-    prod_2 = _mm_mul_ps(b_1, c_2);
-    sum_1 = _mm_hadd_ps(prod_1, prod_2);
-    prod_1 = _mm_mul_ps(b_2, c_1);
-    prod_2 = _mm_mul_ps(b_2, c_2);
-    sum_2 = _mm_hadd_ps(prod_1, prod_2);
-    a_vec = _mm_hadd_ps(sum_1, sum_2);
-    _mm_store_ps(&a[i], a_vec);
+    b_vector_1 = _mm_setr_ps(b[i-1], b[i], b[i+1], b[i+2]);
+    b_vector_2 = _mm_setr_ps(b[i+1], b[i+2], b[i+3], b[i+4]);
+    product_1 = _mm_mul_ps(b_vector_1, c_vector_1);
+    product_2 = _mm_mul_ps(b_vector_1, c_vector_2);
+    sum_1 = _mm_hadd_ps(product_1, product_2);
+    product_1 = _mm_mul_ps(b_vector_2, c_vector_1);
+    product_2 = _mm_mul_ps(b_vector_2, c_vector_2);
+    sum_2 = _mm_hadd_ps(product_1, product_2);
+    a_vector = _mm_hadd_ps(sum_1, sum_2);
+    _mm_store_ps(&a[i], a_vector);
   }
 
   for ( int i = 1020; i < 1023; i++ ) {
